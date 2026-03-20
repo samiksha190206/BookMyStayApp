185aaf2ca033389d13be445ec70b0f8db4f26e8a
samiksha190206 <vv4976@srmist.edu.in>
2026-03-20 09:45:26 +0530

    UC1: Application Entry & Welcome Message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,12 @@
+
 public class BookMyStayApp {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Book My Stay!");
+        System.out.println("Application Name: Book My Stay Hotel Booking System");
+        System.out.println("Version: 1.0");
+
+
+        System.out.println("Application execution completed successfully.");
+    }
 }
