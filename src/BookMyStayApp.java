a1d41cae6f41aa3e31a9afe104df7df7cb2b7093
samiksha190206 <vv4976@srmist.edu.in>
2026-03-20 10:11:17 +0530

    UC2: Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,12 +1,82 @@
 
 public class BookMyStayApp {
+
+    // Abstract Room class
+    abstract static class Room {
+        private String roomType;
+        private int numberOfBeds;
+        private double size;
+        private double pricePerNight;
+
+        public Room(String roomType, int numberOfBeds, double size, double pricePerNight) {
+            this.roomType = roomType;
+            this.numberOfBeds = numberOfBeds;
+            this.size = size;
+            this.pricePerNight = pricePerNight;
+        }
+
+        public String getRoomType() {
+            return roomType;
+        }
+
+        public int getNumberOfBeds() {
+            return numberOfBeds;
+        }
+
+        public double getSize() {
+            return size;
+        }
+
+        public double getPricePerNight() {
+            return pricePerNight;
+        }
+
+        public void displayRoomDetails() {
+            System.out.println("Room Type: " + roomType);
+            System.out.println("Beds: " + numberOfBeds);
+            System.out.println("Size: " + size + " sqm");
+            System.out.println("Price per Night: $" + pricePerNight);
+        }
+    }
+    static class SingleRoom extends Room {
+        public SingleRoom() {
+            super("Single Room", 1, 20.0, 50.0);
+        }
+    }
+
+    static class DoubleRoom extends Room {
+        public DoubleRoom() {
+            super("Double Room", 2, 30.0, 80.0);
+        }
+    }
+
+    static class SuiteRoom extends Room {
+        public SuiteRoom() {
+            super("Suite Room", 3, 50.0, 150.0);
+        }
+    }
+
     public static void main(String[] args) {
+        int singleRoomAvailable = 5;
+        int doubleRoomAvailable = 3;
+        int suiteRoomAvailable = 2;
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+        System.out.println("Welcome to Book My Stay - Room Overview (v2.1)\n");
 
-        System.out.println("Welcome to Book My Stay!");
-        System.out.println("Application Name: Book My Stay Hotel Booking System");
-        System.out.println("Version: 1.0");
+        singleRoom.displayRoomDetails();
+        System.out.println("Available: " + singleRoomAvailable + "\n");
 
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleRoomAvailable + "\n");
 
-        System.out.println("Application execution completed successfully.");
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available: " + suiteRoomAvailable + "\n");
+
+        System.out.println("Room initialization completed.");
     }
 }
+
+
+
